137aedf7ce2c3f2a7f2b2dd9dba316621a520803
Taharina Tasnim <tazreentasnim@gmail.com>
2023-04-02 07:35:29 -0400

    [Issue] JS Projects [Reason] practice [Description] N/A [Author] Tahrina Tasnim

---
+++ HelloWorld/index.js
@@ -0,0 +1,77 @@
+// // print
+// console.log('hello world')
+
+// // variable
+// name = 'shitty brain'
+// console.log(name);
+
+// // user input
+// name = prompt('what kind of fruit you ate?')
+// console.log(name)
+// alert(name)
+
+// // function
+// function f(d, m, y) {
+//     console.log(d + m + y);
+//     alert(m + '/' + d + '/' + y)
+// }
+
+// // arrow function
+// const f = (d, m, y) => {
+//     console.log(d + m + y);
+//     alert(m + '/' + d + '/' + y)
+// }
+// day = Number(prompt('date?'))
+// month = Number(prompt('month?'))
+// year = Number(prompt('year?'))
+
+// f(day, month, year)
+
+// // array
+// arr = [1,2,3,4,5]
+// arr.push(6)
+// arr.push(10)
+// console.log(arr)
+// console.log(arr.slice(2,5))
+// console.log(arr.indexOf(3))
+
+// // objects
+// const person = {
+//     name: 'Misha',
+//     shirt: 'Black',
+//     house: 2,
+//     business: 3,
+//     salary: function (a, b) {
+//         console.log(this.house + this.business , a*b)
+//     }
+// }
+
+// console.log(person)
+// console.log(person.salary(2,5))
+
+// // for loop
+// alphabet = ['a','b','c','d','e','f','.','.','.','x','y','z']
+// for (char of alphabet)
+//     console.log(char)
+
+// const letterCount = (phrase) => {
+//     console.log(phrase.length)
+// }
+// letterCount('hi misha, how are you?')
+
+// // check letter frequency
+// const letterFreq = (phrase) => {
+//     freq = {}
+//     for (letter of phrase) {
+//         if (letter in freq)
+//             freq[letter]++;
+//         else
+//             freq[letter] = 1;
+//     }
+//     return freq
+// }
+// console.log(letterFreq('hi misha, how are you?'))
+
+// [].map() & [].filter() & reduce
+
+// DOM = document object model manipulation
