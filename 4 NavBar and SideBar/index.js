75d4caaeae86af37d82911a1412365d19d4770a0
Taharina Tasnim <tazreentasnim@gmail.com>
2023-04-18 02:33:18 -0400

    [Issue] Banner and Modal added [Reason] Practice [Description] N/A [Author] Tahrina Tasnim

--- 4 NavBar and SideBar/index.js
+++ 4 NavBar and SideBar/index.js
@@ -6,6 +6,10 @@ const sideBarToggle = document.querySelector('.sidebar-toggle')
 const sideBar = document.querySelector('.sidebar')
 const closeBtn = document.querySelector('.close-btn')
 
+const modal = document.querySelector('.modal-overlay')
+const openModalBtn = document.querySelector('.btn-open-modal')
+const closeModalBtn = document.querySelector('.btn-close-modal')
+
 navToggle.addEventListener('click', function() {
     console.log(links.classList.toggle('show-links'))
 })
@@ -14,4 +18,10 @@ sideBarToggle.addEventListener('click', function() {
 })
 closeBtn.addEventListener('click', function() {
     console.log(sideBar.classList.remove('show-sidebar'))
+})
+openModalBtn.addEventListener('click', function() {
+    console.log(modal.classList.add('open-modal'))
+})
+closeModalBtn.addEventListener('click', function() {
+    console.log(modal.classList.remove('open-modal'))
 })
